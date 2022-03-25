2257be819e9cf77255249bd7f9d8e3205fe44002
Christian Maioli <mmchristian@gmail.com>
2022-03-25 00:48:09 -0300

    Add screp and grab lastreplay data

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { QIcon, QSystemTrayIcon, QMenu, QAction } from '@nodegui/nodegui';
 import { resolve } from 'path';
+// @ts-ignore
+import { getRepData } from 'screptools/packages/file/index';
 import http from 'http';
 
 const tray = setupTrayIcon();
@@ -8,10 +10,14 @@ setupWebServer();
 
 (global as any).tray = tray; // prevents garbage collection of tray
 
-
-function setupWebServer() {
+async function setupWebServer() {
   const hostname = '127.0.0.1';
   const port = 3000;
+  
+  const homedir = require("os").homedir();
+
+  const repdata = await getRepData(homedir + '\\Documents\\StarCraft\\Maps\\Replays\\LastReplay.rep');
+  console.log(repdata);
 
   const server = http.createServer((req: any, res: any) => {
     res.statusCode = 200;
